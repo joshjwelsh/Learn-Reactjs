7c3ccab6a8a966acc5f7f0204749ed1e9db0db84
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-06-25 15:35:00 -0400

    clearing app component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import './App.css';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  // const person = {name : 'Josh', age : 30};
-  const link = "http://www.google.com";
   return (
     <div className="App">
-    <div className="content">
-      <h1>{ title }</h1>
-      <p>Liked { likes } times </p>
-      {/* <p> { person } </p> */}
-      <p> {10} </p>
-      <p> {'Hello Ninjas'} </p>
-      <p> {[1,2,3,4,5]} </p>
-      <p> { Math.random() * 10}</p>
-      <a href={ link }>google site</a>
-    </div> 
+    
     </div>
   );
 }
