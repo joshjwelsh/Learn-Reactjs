99b2424249a61f42d78d409ebf821fde764f0916
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-06-25 16:23:54 -0400

    Handle funcs, anonymous functions and event object

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+	const handleClick = () => {
+		console.log('Hello, joshua');
+	}
+	const handleClickAgain = (name, e) =>{
+		console.log('hello ' + name + e);
+	} 
+
 	return (  
 		<div className="home">
 			<h2>Homepage</h2>
+			<button onClick={handleClick}>Click me</button>
+			<button onClick={ (e) => handleClickAgain('mario', e)}> Click me again </button>
 		</div>
 	);
 
