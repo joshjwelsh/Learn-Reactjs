2be24381a65ea65c25c2d86160b39b46dd562bca
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-06-26 00:23:58 -0400

    added error handling for failed fetch

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,32 @@ import { useState, useEffect } from 'react';
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
 	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		setTimeout(() =>{
-				fetch("http://localhost:8000/blogs").then(res => {
+				fetch("http://localhost:8001/blogss").then(res => {
+				if (!res.ok){
+					throw Error('could not fetch the data for that resources')
+				}
 				return res.json();
 			}).then((data) => {
 				console.log(data);
 				setBlogs(data);
 				setIsPending(false);
-			});
+				setError(null);
+
+			})
+			.catch(err => {
+				setIsPending(false);
+				setError(err.message);
+			})
 		}, 1000 );
 	},[])
 	return (  
 		<div className="home">	
+			{error && <div> {error} </div>}
 			{isPending && <div> Loading...</div>}
-
 			{blogs && <BlogList blogs={blogs} title="All blogs" />}
 
 		</div>
