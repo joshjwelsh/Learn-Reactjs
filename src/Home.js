58c65f56d28e072f1bf34010d102da17c38e6402
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-06-26 00:13:50 -0400

    add loading screen with conditional template

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,27 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
+	const [isPending, setIsPending] = useState(true);
 
 	useEffect(() => {
-		fetch("http://localhost:8000/blogs").then(res => {
-			return res.json()
-		}).then((data) => {
-			console.log(data)
-			setBlogs(data)
-		})
+		setTimeout(() =>{
+				fetch("http://localhost:8000/blogs").then(res => {
+				return res.json();
+			}).then((data) => {
+				console.log(data);
+				setBlogs(data);
+				setIsPending(false);
+			});
+		}, 1000 );
 	},[])
 	return (  
-		<div className="home">
+		<div className="home">	
+			{isPending && <div> Loading...</div>}
+
 			{blogs && <BlogList blogs={blogs} title="All blogs" />}
+
 		</div>
 	);
 
