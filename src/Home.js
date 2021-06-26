378205e132d30ab0d67ae46d79b4aa9660a2493f
Joshua Welsh <joshua.welsh1001@gmail.com>
2021-06-25 23:46:16 -0400

    conditional templating and fetching data using useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,23 +2,19 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{title: 'My new website', body: 'lorem ipsum...', author:'mario', id:1},
-		{title: 'Welcome party!', body: 'lorem ipsum...', author:'yoshi', id:2},
-		{title: 'Web dev tops tips', body: 'lorem ipsum...', author:'mario', id:3}
-	]);
-
-	const handleDelete = (id) => {
-		const newBlogs = blogs.filter(blog => blog.id !== id);
-		setBlogs(newBlogs)
-	}
+	const [blogs, setBlogs] = useState(null);
 
 	useEffect(() => {
-		console.log('Use effect ran');
+		fetch("http://localhost:8000/blogs").then(res => {
+			return res.json()
+		}).then((data) => {
+			console.log(data)
+			setBlogs(data)
+		})
 	},[])
 	return (  
 		<div className="home">
-			<BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+			{blogs && <BlogList blogs={blogs} title="All blogs" />}
 		</div>
 	);
 
